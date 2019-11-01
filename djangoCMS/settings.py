01407ce9fc1b0132070c253d12eb551ead537ab0
Alamin <alamincmt7418@gmail.com>
2019-11-01 12:08:05 +0600

    updatedFile

--- djangoCMS/settings.py
+++ djangoCMS/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT= os.path.join(os.path.dirname(BASE_DIR), "static")
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT= os.path.join(os.path.dirname(BASE_DIR), "media")
+MEDIA_ROOT= os.path.join(BASE_DIR, "media")
 
 STATICFILES_DIRS = [
 os.path.join(BASE_DIR, "static"),
